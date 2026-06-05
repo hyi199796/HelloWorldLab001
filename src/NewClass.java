2600653c40945c7bbba0bf05d99c8b0a984a99e4
student <student@203-67>
2026-06-05 11:11:34 +0800

    BBB

--- src/NewClass.java
+++ src/NewClass.java
@@ -3,7 +3,7 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Java.");
-		System.out.println("I am 411305386");
+		System.out.println("411305386");
 
 	}
 
